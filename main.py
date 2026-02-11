b71525cfe7359ace4d9fddd554c16da5239fb8b5
sUmi7ro <simonto.no@outlook.com>
2026-02-11 16:34:11 +0100

    Comment/Note for future me

--- main.py
+++ main.py
@@ -48,6 +48,12 @@ output_wind_width = system_width if system_width < int(cap.get(cv2.CAP_PROP_FRAM
 cv2.namedWindow('Yolo vision', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('Yolo vision', output_wind_width, output_wind_height)
 
+# NOTE (til når vi skal adde OCR):
+# HVis vi går med å bruke dedikert kamera for REID så burde vi
+# bare gjøre OCR(hjelm)/REID på en viss del av input bildene, så ikke hele.
+# + ha store mellomrom mellom hver gang
+# (for hver N detection innenfor en spesifik del av bilde)
+
 # Vi kan bruke ByteTrack her også, merket at det gikk litt raskere.
 tracker = BotSort(reid_weights=Path('osnet_x0_25_msmt17.pt'), device='cpu', half=False, track_high_thresh=0.65)
 
